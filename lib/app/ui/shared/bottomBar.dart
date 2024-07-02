8100177d0ad2bc5aaa93a319fc141ba78a72c57c
PatrickMamsery <pkrobert1612@gmail.com>
2024-07-02 11:43:15 +0300

    wip: miscellaneous fixes

--- lib/app/ui/shared/bottomBar.dart
+++ lib/app/ui/shared/bottomBar.dart
@@ -10,17 +10,19 @@ class BottomBar extends StatelessWidget {
   BottomBar({Key? key, required this.userId}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Obx(() => BottomNavigationBar(
-          currentIndex: controller.currentIndex.value,
-          selectedItemColor: Colors.blue,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Report"),
-            BottomNavigationBarItem(icon: Icon(Icons.list), label: "List"),
-            // BottomNavigationBarItem(
-            //     icon: Icon(Icons.schedule), label: "Schedule"),
-          ],
-          onTap: (index) => controller.changePage(index, userId),
-          type: BottomNavigationBarType.fixed,
-        ));
+    return Obx(
+      () => BottomNavigationBar(
+        currentIndex: controller.currentIndex.value,
+        selectedItemColor: Colors.blue,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Report"),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: "List"),
+          // BottomNavigationBarItem(
+          //     icon: Icon(Icons.schedule), label: "Schedule"),
+        ],
+        onTap: (index) => controller.changePage(index, userId),
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
   }
 }
